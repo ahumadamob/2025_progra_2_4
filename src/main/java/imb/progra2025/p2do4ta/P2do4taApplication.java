12e99dfe1d15085d8cd96df104289c8fea05c275
Mario Ahumada <ahumadamob@gmail.com>
2025-05-16 13:33:39 -0300

    First commit

---
+++ src/main/java/imb/progra2025/p2do4ta/P2do4taApplication.java
@@ -0,0 +1,13 @@
+package imb.progra2025.p2do4ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2do4taApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2do4taApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imb/progra2025/p2do4ta/P2do4taApplicationTests.java
@@ -0,0 +1,13 @@
+package imb.progra2025.p2do4ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2do4taApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
