da2c8dd4dc124044e8c41a345478623e9759231b
Mario Ahumada <ahumadamob@gmail.com>
2025-06-06 20:37:56 -0300

    Primera clase de Controladores

---
+++ src/main/java/imb/progra2025/p2do4ta/Mensaje.java
@@ -0,0 +1,22 @@
+package imb.progra2025.p2do4ta;
+
+public class Mensaje {
+	
+	private int codigo;
+	private String contenido;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	
+	
+
+}
